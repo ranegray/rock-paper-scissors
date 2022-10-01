1ef73c01351bf43accea0c3f20ec453e0deaabce
RaneGray <RaneGray@outlook.com>
2022-10-01 16:41:06 +0200

    Initial commit

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const getComputerChoice = () => {
+    let choice = Math.floor(Math.random() * 3);
+
+    switch (choice) {
+        case 0:
+            return 'Rock';
+            break;
+        case 1:
+            return 'Paper';
+            break;
+        case 2:
+            return 'Scissors';
+            break;
+    };
+
+}
 
